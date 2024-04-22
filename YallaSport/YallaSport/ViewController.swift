a22e0cf7737691ba626a8b510581a94b57132734
Marwaaboalata <113009464+Marwaaboalata@users.noreply.github.com>
2024-04-23 00:00:05 +0200

    splash Screen with lotfy

--- YallaSport/YallaSport/ViewController.swift
+++ YallaSport/YallaSport/ViewController.swift
@@ -4,16 +4,37 @@
 //
 //  Created by husayn on 22/04/2024.
 //
+//
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    var animationView : LottieAnimationView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        animationView = LottieAnimationView(name: "mySplashLotti")
+        animationView?.frame = view.bounds
+        animationView?.animationSpeed = 0.9
+        animationView?.contentMode = .scaleAspectFit
+        animationView?.loopMode = .loop
+        view.addSubview(animationView!)
+        animationView?.play()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3){
+            
+            if let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController {
+ 
+                self.navigationController?.pushViewController(homeViewController, animated: true)
+            }
+            
+        }
+        
+        
     }
-
 
 }
 
