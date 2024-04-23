9b21692d7943d2b9a042aad1ae22721675f4795b
Marwaaboalata <113009464+Marwaaboalata@users.noreply.github.com>
2024-04-23 18:46:46 +0200

    Home Screen

---
+++ YallaSport/YallaSport/HomeSportsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  HomeSportsCollectionViewCell.swift
+//  YallaSport
+//
+//  Created by marwa on 23/04/2024.
+//
+
+import UIKit
+
+class HomeSportsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var sportsImageView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var sportsNameLabel: UILabel!
+}
--- YallaSport/YallaSport/HomeViewController.swift
+++ YallaSport/YallaSport/HomeViewController.swift
@@ -5,25 +5,49 @@
 //  Created by marwa on 22/04/2024.
 //
 
+
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController
+,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource,UICollectionViewDelegate{
 
+    var sportsArray :[String]=[]
+    
+    @IBOutlet weak var myCollectionViewSportsType: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        sportsArray =   ["football","basketball","tennis","cricket"]
+        self.myCollectionViewSportsType.delegate = self
+        self.myCollectionViewSportsType.dataSource = self
+        
+        self.myCollectionViewSportsType.layer.borderWidth = 1.0
+        self.myCollectionViewSportsType.layer.borderColor = UIColor.purple.cgColor
+        self.myCollectionViewSportsType.layer.cornerRadius = 50
+        
+    }
+    
+ 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! HomeSportsCollectionViewCell
+        cell.sportsImageView.image = UIImage(named: sportsArray[indexPath.item])
+        cell.sportsNameLabel.text = sportsArray[indexPath.item].capitalized
+       
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 150, height: 240)
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(sportsArray[indexPath.item])
     }
-    */
-
 }
+
