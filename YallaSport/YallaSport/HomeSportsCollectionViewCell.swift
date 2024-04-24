b1853d778f3ef80958583433a013bb4912c85f93
Marwaaboalata <113009464+Marwaaboalata@users.noreply.github.com>
2024-04-24 22:57:07 +0200

    Home screen

---
+++ YallaSport/YallaSport/HomeSportsCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeSportsCollectionViewCell.swift
+//  YallaSport
+//
+//  Created by marwa on 24/04/2024.
+//
+
+import UIKit
+
+class HomeSportsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sportLabel: UILabel!
+    
+    @IBOutlet weak var sportImageView: UIImageView!
+    
+    
+
+    
+}
--- YallaSport/YallaSport/LeagueViewController.swift
+++ YallaSport/YallaSport/LeagueViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    var sportStr = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Network.getInstance()?.fetchLeagues(sportType: sportStr, completionHandler: { [weak self] result in
+            switch result {
+            case .success(let leagues) :
+                DispatchQueue.main.async {
+                  //  self?.myLabel.text = leagues.result[0].league_name
+                    print(leagues.result[0].league_name)
+                }
+                
+            case .failure(let error) :
+                print("error : ",error)
+            }
+
+        })
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
